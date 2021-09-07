a750cd4639db648645e63d12260b59608f474ff4
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2021-09-07 17:18:53 +0900

    s

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ function Game_load(width,height){
       var blink = 50;
       var blinks = 0;
       var Image = [];
+      var Stone = false;
 
       function Images(a,b){
         Image[i] = new Sprite();
@@ -34,11 +35,18 @@ function Game_load(width,height){
       function Buttons(x,y,a,i){
         Ui_Button[i] = new Button(a,"light",width/4,height/10);
         if(i==0)Ui_Button[i].moveTo(x,y);
-        else Ui_Button[i].moveTo(x+width,y+height);
+        else{
+          Ui_Button[i].moveTo(x+width,y+height);
+          Ui_Button[i].opacity = 0.5;
+        }
         Ui_Button[i]._style["font-size"] = height/20;
         scene.addChild(Ui_Button[i]);
         Ui_Button[i].addEventListener("touchstart",function(e){
           switch(Ui_Button[i]._text){
+            case "コマ送り":
+              move = true;
+              return;
+              break;
             case "まばたき間隔+":
               blinks = 1;
               return;
@@ -96,8 +104,10 @@ function Game_load(width,height){
             case "髪":
               Image[5].imageurl++;
               if(Image[5].imageurl==11) Image[5].imageurl = 1;
-              Image[5]._element.src = "image/hair/"+Image[5].imageurl+".png";
-              Image[9]._element.src = "image/bangs/"+Image[5].imageurl+".png";
+              if(!Stone){
+                Image[5]._element.src = "image/hair/"+Image[5].imageurl+".png";
+                Image[9]._element.src = "image/bangs/"+Image[5].imageurl+".png";
+              }
               return;
               break;
             case "眼":
@@ -137,9 +147,33 @@ function Game_load(width,height){
               Ui_Button[i]._text = "涙オフ";
               return;
               break;
-            case "時間":
-              if(move) move = false;
-              else move = true;
+            case "首石化":
+              Stone = Image[5].imageurl;
+              Image[5]._element.src = "image/hair/stone.png";
+              Image[9]._element.src = "image/bangs/stone.png";
+              Ui_Button[i]._text = "溶かす";
+              return;
+              break;
+            case "溶かす":
+              Image[5]._element.src = "image/hair/"+Stone+".png";
+              Image[9]._element.src = "image/bangs/"+Stone+".png";
+              Ui_Button[i]._text = "首石化";
+              return;
+              break;
+            case "停":
+              move = false;
+              Ui_Button[19].moveTo(Ui_Button[19].x-width,Ui_Button[19].y-height);
+              Ui_Button[i]._text = "動";
+              return;
+              break;
+            case "動":
+              move = true;
+              Ui_Button[19].moveTo(Ui_Button[19].x+width,Ui_Button[19].y+height);
+              Ui_Button[i]._text = "停";
+              return;
+              break;
+            case "コマ送り":
+              move = false;
               return;
               break;
             case "まばたき間隔+":
@@ -167,43 +201,46 @@ function Game_load(width,height){
         });
       }
 
-      scene.addEventListener("enterframe",function(){
-        if(move){
-          Image[2].imageurl++;
-          if(Image[2].imageurl==13) Image[2].imageurl = 1;
-          Image[2]._element.src = "image/body/"+Image[2].imageurl+".png";
-          if(time_H){
-            Image[1].x += 1;
-            Image[4].x += 1;
-          }
-          else{
-            Image[1].x -= 1;
-            Image[4].x -= 1;
-          }
-          if(time%5==0){
-            if(time_H) time_H = false;
-            else time_H = true;
-          }
-          if(blink != 3){
-            if(Image[8].imageurl==4){
-              Image[8].imageurl = 1;
-              Image[8]._element.src = "image/eyelashes/1.png";
-            }
-            if(Image[8].imageurl==3){
-              Image[8].imageurl = 4;
-              Image[8]._element.src = "image/eyelashes/2.png";
-            }
-            if(Image[8].imageurl==2){
-              Image[8].imageurl = 3;
-              Image[8]._element.src = "image/eyelashes/3.png";
-            }
-            if(time%blink==0){
-              Image[8].imageurl = 2;
-              Image[8]._element.src = "image/eyelashes/2.png";
-            }
-          }
-          time++;
+      function One_Scene(){
+        Image[2].imageurl++;
+        if(Image[2].imageurl==13) Image[2].imageurl = 1;
+        Image[2]._element.src = "image/body/"+Image[2].imageurl+".png";
+        if(time_H){
+          Image[1].x += 1;
+          Image[4].x += 1;
         }
+        else{
+          Image[1].x -= 1;
+          Image[4].x -= 1;
+        }
+        if(time%5==0){
+          if(time_H) time_H = false;
+          else time_H = true;
+        }
+        if(blink != 3){
+          if(Image[8].imageurl==4){
+            Image[8].imageurl = 1;
+            Image[8]._element.src = "image/eyelashes/1.png";
+          }
+          if(Image[8].imageurl==3){
+            Image[8].imageurl = 4;
+            Image[8]._element.src = "image/eyelashes/2.png";
+          }
+          if(Image[8].imageurl==2){
+            Image[8].imageurl = 3;
+            Image[8]._element.src = "image/eyelashes/3.png";
+          }
+          if(time%blink==0){
+            Image[8].imageurl = 2;
+            Image[8]._element.src = "image/eyelashes/2.png";
+          }
+        }
+        time++;
+        return;
+      }
+
+      scene.addEventListener("enterframe",function(){
+        if(move) One_Scene();
         if(!(blink == 3 && blinks== -1)) blink += blinks;
         if (game.input.down && Image[0].y < 20){
             Image[0].y+=1;
@@ -244,7 +281,7 @@ function Game_load(width,height){
        Buttons(width/4*1,height/10*6,"眉",6);
        Buttons(width/4*2,height/10*6,"眼",7);
        Buttons(width/4*3,height/10*6,"ハイライトオフ",8);
-       Buttons(0,height/10*5,"時間",9);
+       Buttons(0,height/10*5,"停",9);
        Buttons(width/4*1,height/10*5,"まばたき間隔+",10);
        Buttons(width/4*2,height/10*5,"まばたき間隔-",11);
        Buttons(width/4*3,height/10*5,"頬",12);
@@ -254,6 +291,9 @@ function Game_load(width,height){
        Buttons(0,height/10*8,"左腕",16);
        Buttons(width/4*1,height/10*8,"右腕",17);
        Buttons(width/4*3,height/10*7,"リセット",18);
+       Buttons(width/4*3,height/10*8,"コマ送り",19);
+       Buttons(0,height/10*9,"首石化",20);
+       Ui_Button[19].moveTo(Ui_Button[19].x+width,Ui_Button[19].y+height);
        return scene;
     };
     game.replaceScene(Main_Scene());
