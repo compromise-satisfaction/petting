d04cada45d26de1ba5415d4db318cb3031e14423
compromise-satisfaction <44921951+compromise-satisfaction@users.noreply.github.com>
2021-09-09 22:43:24 +0900

    a

--- js/main.js
+++ js/main.js
@@ -111,7 +111,7 @@ function Game_load(width,height){
               break;
             case "髪":
               Image[5].imageurl++;
-              if(Image[5].imageurl==12) Image[5].imageurl = 1;
+              if(Image[5].imageurl==14) Image[5].imageurl = 1;
               Image[5]._element.src = "image/hair/"+Image[5].imageurl+".png";
               Image[9]._element.src = "image/bangs/"+Image[5].imageurl+".png";
               return;
